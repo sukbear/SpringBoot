d22effcd3763404e75655ff9dead38f19ed9a9c5
liufa <704759810@qq.com>
2018-11-16 18:50:37 +0800

    springboot

---
+++ springboot_test/src/main/java/cn/test/App.java
@@ -0,0 +1,13 @@
+package cn.test;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("cn.test.mapper")
+public class App {
+public static void main(String[] args) {
+	SpringApplication.run(App.class, args);
+}
+}
---
+++ springboot_test/src/main/java/cn/test/mapper/UsersMapper.java
@@ -0,0 +1,14 @@
+package cn.test.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import cn.test.pojo.Users;
+public interface UsersMapper {
+	void insertUser(Users users);
+	List<Users> selectUsersAll();
+	Users selectUsersById(Integer id);
+	void updateUser(Users users);
+	void deleteUserById(Integer id);
+}
